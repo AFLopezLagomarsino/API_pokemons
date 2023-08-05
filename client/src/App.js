058a0c8c7ef1dd7a426a17246b8ebdbcbeeed456
AFLopezLagomarsino <andresinfernoxii@gmail.com>
2023-08-05 03:48:48 -0300

    rutas agregadas, completar about o removerlo si no se llega con el tiempo

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import {Routes, Route} from "react-router-dom"
 import LandingPage from './components/LandingPage/LandingPage';
 import Home from "./components/Home/Home"
+import Form from "./components/Form/Form"
+import Detail from "./components/Detail/Detail"
 function App() {
   return (
     <div className="App">
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage/>}/>
         <Route path="/home" element={<Home/>}/>
+        <Route path="/pokemon" element={<Form/>}/>
+        <Route path='/detail/:id' element={<Detail/>}/>
       </Routes>
     </div>
   );
