048683e3aaaf36df14da6cba1a28391403577dd1
AFLopezLagomarsino <andresinfernoxii@gmail.com>
2023-08-16 23:20:26 -0300

    error corregido en create pokemon

--- api/src/controllers/pokemons/createPokemon.js
+++ api/src/controllers/pokemons/createPokemon.js
@@ -2,7 +2,7 @@ const { Pokemon, Type } = require ("../../db")
 
 const createPokemon = async (name,image,height,weight,attack,health,speed,defense,types) =>{
     if (!name & !image & !height & !weight & !attack & !health & !speed & !defense & !types){
-        return resizeBy.status(400).send("Faltan datos")
+        return res.status(400).send("Missing data")
     }
     let newPokemon = await Pokemon.create({name,image,height,weight,attack,health,speed,defense,types})
     const typesDb = await Type.findAll({where:{name:types}})
